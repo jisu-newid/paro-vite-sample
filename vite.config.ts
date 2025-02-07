a3d24558eaadcf01f1e3c501e58f2f58733f9ad8
jisu-newid <jisu@its-newid.com>
2025-02-07 17:21:44 +0900

    :tada: first commit

---
+++ vite.config.ts
@@ -0,0 +1,31 @@
+import react from '@vitejs/plugin-react-swc';
+import { defineConfig } from 'vite';
+import legacy from '@vitejs/plugin-legacy';
+
+// https://vite.dev/config/
+export default defineConfig({
+    resolve: {
+        alias: [{ find: '@', replacement: '/src' }]  // 절대경로 설정
+    },
+    plugins: [
+        react(),
+        legacy({
+            targets: ['chrome >= 69'],
+            polyfills: true,
+            renderLegacyChunks: false   // 단일 번들파일 생성
+        })
+    ],
+    build: {
+        cssTarget: 'chrome69',
+        outDir: 'dist',
+        rollupOptions: {
+            input: {
+                main: 'index.html'
+            },
+            output: {
+                entryFileNames: 'bundle.js',
+                format: 'iife'
+            }
+        }
+    }
+});
